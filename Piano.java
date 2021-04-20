3a1aa0aee916fdd4f80bb3a0fae0aaf2aeefc822
thane51105 <thane.hudelson@icloud.com>
2021-04-20 08:29:05 -0400

    placed the keys on the piano

--- Piano.java
+++ Piano.java
@@ -14,5 +14,16 @@ public class Piano extends World
     public Piano() 
     {
         super(800, 340, 1);
+        makeKeys();
+    }
+    /**
+     * create the piano keys
+     */
+    public void makeKeys()
+    {
+        for (int i=0; i<12; i++)
+        {
+            addObject (new Key ("g", "3a.wav"), i*63 + 54, 140);
+        }
     }
 }
