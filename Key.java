c6baf9ee64254f4023d3cfeeb90ffcc8e59d4402
thane51105 <thane.hudelson@icloud.com>
2021-04-27 08:38:36 -0400

    keys are able to be customized and now have assigned keys and sounds

--- Key.java
+++ Key.java
@@ -5,13 +5,19 @@ public class Key extends Actor
     private boolean isDown;
     private String key;
     private String sound;
+    private String upImage;
+    private String downImage;
     /**
      * Create a new key.
      */
-    public Key(String keyName, String soundFile)
+    public Key(String keyName, String soundFile, String img1, String img2)
     {
+        isDown = false;
         key = keyName;
         sound = soundFile;
+        upImage = img1;
+        downImage = img2;
+        setImage( upImage );
     }
 
     /**
@@ -21,13 +27,13 @@ public class Key extends Actor
     {
         if( !isDown && Greenfoot.isKeyDown(key))
         {
-            setImage("white-key-down.png");
+            setImage(downImage);
             play ();
             isDown = true;
         }
         if( isDown && !Greenfoot.isKeyDown(key))
         {
-            setImage("white-key.png");
+            setImage(upImage);
             isDown = false;
         }
     }
--- Piano.java
+++ Piano.java
@@ -8,6 +8,10 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, and Greenfoot)
  */
 public class Piano extends World
 {
+    private String[]whiteKeys =
+        {"a", "s", "d", "f", "g", "h", "j", "k", "l", ";", "'", "\\"};
+    private String[]whiteNotes = 
+        { "3c", "3d", "3e", "3f", "3g", "3a", "3b", "4c", "4d", "4e", "4f", "4g" };
     /**
      * Make the piano.
      */
@@ -21,9 +25,11 @@ public class Piano extends World
      */
     public void makeKeys()
     {
-        for (int i=0; i<12; i++)
+        int i;
+        for (i=0; i < whiteKeys.length;  i++)
         {
-            addObject (new Key ("g", "3a.wav"), i*63 + 54, 140);
+             Key key = new Key (whiteKeys[i],  whiteNotes[i] + ".wav", "white-key.png", "white-key-down.png");
+             addObject (key,  54  +  i*63,  140);
         }
     }
 }
