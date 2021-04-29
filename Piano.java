2b84b66b019f3900605de7a9586e42a3af297110
thane51105 <thane.hudelson@icloud.com>
2021-04-29 08:23:40 -0400

    added sharp keys, completed piano

--- Piano.java
+++ Piano.java
@@ -12,6 +12,10 @@ public class Piano extends World
         {"a", "s", "d", "f", "g", "h", "j", "k", "l", ";", "'", "\\"};
     private String[]whiteNotes = 
         { "3c", "3d", "3e", "3f", "3g", "3a", "3b", "4c", "4d", "4e", "4f", "4g" };
+    private String[]blackKeys=
+        {"W", "E", "", "T", "Y", "U", "", "O", "P", "","]"};
+    private String[]blackNotes=
+        {"3c#", "3d#", "", "3f#", "3g#", "3a#", "", "4c#", "4d#", "", "4f#"};
     /**
      * Make the piano.
      */
@@ -30,6 +34,14 @@ public class Piano extends World
         {
              Key key = new Key (whiteKeys[i],  whiteNotes[i] + ".wav", "white-key.png", "white-key-down.png");
              addObject (key,  54  +  i*63,  140);
+        }
+        for (i=0; i < whiteKeys.length-1; i++)
+        {
+            if( ! blackKeys[i].equals(""))
+            {
+                Key key = new Key(blackKeys[i], blackNotes[i]+".wav","black-key.png","black-key-down.png");
+                addObject(key, 85 + (i*63), 86);
+            }
         }
     }
 }
